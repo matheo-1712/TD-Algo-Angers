43028db18c3aa49ab7dccef87165a4b2a12acc32
matheo-1712 <perod.matheo@gmail.com>
2025-09-01 21:57:46 +0200

    Correction bug exo 5

--- TD1/ex5.cpp
+++ TD1/ex5.cpp
@@ -1,27 +1,33 @@
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <iostream>
-#include <ostream>
-
-using namespace std;
+#include <limits>
 
 void ex5() {
     int reponse = 0;
 
     std::srand(std::time(nullptr));
     int secret = std::rand() % 100 + 1;
-    while (reponse == 0 || reponse != secret) {
+
+    while (reponse != secret) {
         std::cout << "Merci de choisir un nombre entre 1 et 100" << std::endl;
         std::cin >> reponse;
 
-        if (reponse == secret) {
-            std::cout << "BRAVO C'EST LE BON CHIFFRE";
-        } else if (reponse > 100 || reponse < 0) {
-            std::cout << "Merci de choisir un chiffre valide";
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Vide la ligne
+            std::cout << "Entrée invalide. Veuillez entrer un nombre entier." << std::endl;
+            continue;
+        }
+
+        if (reponse > 100 || reponse < 1) {
+            std::cout << "Chiffre invalide. ";
         } else if (reponse > secret) {
-            std::cout << "C'est moins";
+            std::cout << "C'est moins " << std::endl;
         } else if (reponse < secret) {
-            std::cout << "C'est plus";
+            std::cout << "C'est plus " << std::endl;
+        } else {
+            std::cout << "BRAVO C'EST LE BON CHIFFRE !" << std::endl;
         }
     }
 }
